5dfba7652962eb8e879a7b14b027d1ece08460d9
Nick Truong <nicktnc24@gmail.com>
2024-07-19 16:34:13 -0500

    fixed errr

--- src/app/(sections)/resume/page.tsx
+++ src/app/(sections)/resume/page.tsx
@@ -6,7 +6,7 @@ export const metadata = {
 export default function Resume(){
     return (
         <div className="min-h-screen flex flex-col items-center">
-          <div>
+
         </div>
       );
 }

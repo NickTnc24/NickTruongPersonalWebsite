047be5c52ddc98ef59beae10a526d0374696892d
nicktnc24 <nicktnc24@tamu.edu>
2024-07-25 13:39:28 -0500

    added textwrapping to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,14 @@ import Link from "next/link";
 
 
 export default function Home() {
+
   return (
     <div className="min-h-screen flex flex-col items-center">
       <Link href = "/" className="text-7xl py-5 mb-10">
           Nick Truong
       </Link>
       {/* <ImageSlider></ImageSlider> */}
-      <div className="flex flex-row items-center gap-96">
+      <div className="flex flex-wrap flex-row justify-center items-center gap-x-96 gap-y-32 w-4/5">
         <Image 
           src = "/_art/headshot.jpg"
           alt = "Logo"
@@ -20,7 +21,9 @@ export default function Home() {
           height = {1}
           className="rounded-full animate-fadeInRight"
         />
-        <div>Hi I&apos;m Nick Truong and I go to TAMU and this my website</div>
+        <div className="w-1/2 about-picture-description">
+          &emsp;&emsp;Welcome to my website! I&apos;m Nick Truong and I am majoring in Computer Science with a minor of Game Design and Cybersecurity at Texas A&M University. I work as a SOC Analyst for Texas A&M University as well as a Recreational Activities Director for SASE.
+        </div>
       </div>
     </div>
   );

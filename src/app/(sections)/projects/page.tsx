2f20d9cd5542fb8df2018f5637a51ed67f7f49be
Nick Truong <nicktnc24@gmail.com>
2024-07-27 14:05:23 -0500

    centering of project stuff

--- src/app/(sections)/projects/page.tsx
+++ src/app/(sections)/projects/page.tsx
@@ -9,7 +9,7 @@ export const metadata = {
 
 function projectLinks(title: string,urlName: string, imgPath: string, description: string, techUsed: Array<String>){
     return(
-        <div className="flex rounded-md dark:bg-darkbackgroundshade bg-lightbackgroundshade flex-wrap lg:flex-nowrap justify-center gap-2 min-w-[250px] max-w-[720px]">
+        <div className="flex rounded-md dark:bg-darkbackgroundshade bg-lightbackgroundshade justify-center flex-wrap lg:flex-nowrap gap-2 min-w-[250px] max-w-[720px]">
             <Link href={urlName} target="_blank" className="shrink-0">
                 <Image
                     src = {imgPath}
@@ -19,7 +19,7 @@ function projectLinks(title: string,urlName: string, imgPath: string, descriptio
                     className="rounded-md"
                 />
             </Link>
-            <div className="flex flex-col justify-around gap-y-2">
+            <div className="flex flex-col justify-around gap-y-2 items-center">
                 <Link href={urlName} target="blank" className="text-2xl">
                     {title}
                 </Link>
@@ -39,10 +39,10 @@ export default function Projects(){
         <div className="min-h-screen flex flex-col gap-10">
           <div className="text-header-size self-center">Projects</div>
           <div className="self-center opacity-60">Click on an project to learn more about it</div>
-          <div className="flex flex-row gap-y-10 mx-10 justify-start gap-x-10 flex-wrap">
-            <div>{projectLinks("SecuroChat", "https://github.com/aggie-coding-club/SecuroChat?tab=readme-ov-file", "/_art/securochat.jpg", "SecuroChat is an end-to-end Encrypted real-time chat application ensuring secure communication between users.", ["React Native", "Node.js", "AWS", "WebSocket", "React Navigation", "PostgreSQL", "Redux", "Firebase Cloud Messaging"])}</div>
+          <div className="flex flex-row gap-y-10 mx-10 justify-center gap-x-10 flex-wrap">
+            <div>{projectLinks("SecuroChat", "https://github.com/aggie-coding-club/SecuroChat", "/_art/securochat.jpg", "SecuroChat is an end-to-end Encrypted real-time chat application ensuring secure communication between users.", ["React Native", "Node.js", "AWS", "WebSocket", "React Navigation", "PostgreSQL", "Redux", "Firebase Cloud Messaging"])}</div>
             <div>{projectLinks("Personal Website", "https://github.com/NickTnc24/NickTnc24.github.io", "/_art/website.png", "I made a personal website that you can find all about me!", ["Next.js", "Tailwind", "Vercel"])}</div>
-            <div>{projectLinks("Personal Website", "https://github.com/NickTnc24/NickTnc24.github.io", "/_art/website.png", "I made a personal website that you can find all about me!", ["Next.js", "Tailwind", "Vercel"])}</div>
+            <div>{projectLinks("Wordle", "https://github.com/NickTnc24/Wordle", "/_art/wordle.png", "I coded Wordle that has a special twist to it! Once you submit a guess, the letters that are not a part of the secret word are disabled to use.", ["JavaFX"])}</div>
           </div>
         </div>
       );

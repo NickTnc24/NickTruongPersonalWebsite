7e928ab905e15147696020cda50f8ffdc9a76a28
Nick Truong <nicktnc24@gmail.com>
2024-07-29 16:50:59 -0500

    changed projects to work

--- src/app/_components/headerComponent.tsx
+++ src/app/_components/headerComponent.tsx
@@ -30,22 +30,10 @@ export default function HeaderComponent(){
                 <div className="flex flex-row p-4 px-7 rounded-md gap-x-20 gap-y-5 max-sm:gap-10 max-sm:text-xl">
                     <div className="hover:scale-110 hover:duration-300">{headerLinks("/", "Home")}</div>
                     <div className="addPing hover:scale-110 hover:duration-300" id= 'aboutPing' onClick={() => removePing('aboutPing')}>{headerLinks("/about", "About")}</div>
-                    <div className="addPing hover:scale-110 hover:duration-300" id= 'projectsPing' onClick={() => removePing('projectsPing')}>{headerLinks("/projects", "Projects")}</div>
+                    <div className="addPing hover:scale-110 hover:duration-300" id= 'projectsPing' onClick={() => removePing('projectsPing')}>{headerLinks("/work", "Work")}</div>
                 </div>
             </div> 
             <div className="relative flex flex-wrap items-center justify-center rounded-2xl left-1/2 w-3/4 gap-3 -translate-x-1/2 mb-10">
-                {/* <Link href = "/" className=" flex-shrink-0">
-                    <Image 
-                        src = "/_art/Logo.png"
-                        alt = "Logo"
-                        width = {100}
-                        height = {1}
-                        className=""
-                    />
-                </Link> */}
-                {/* <Link href = "/" className="text-7xl py-5">
-                    Nick Truong
-                </Link> */}
             </div>
         </>
     );

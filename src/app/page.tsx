c184fe6b0750c572a496ae7edf6f4113f7043b9e
Nick Truong <nicktnc24@gmail.com>
2024-08-19 21:47:19 -0500

    jiojosfjaosd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import React, {useState, useEffect} from 'react';
 import Link from "next/link";
-import HeaderComponent from "./_components/headerComponent";
 
 export default function Home() {
 

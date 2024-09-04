71583fd8887acc8505db7ce83bd757069c96e0d2
Nick Truong <nicktnc24@gmail.com>
2024-09-03 23:58:02 -0500

    updated about

--- src/app/(sections)/about/page.tsx
+++ src/app/(sections)/about/page.tsx
@@ -48,7 +48,7 @@ export default function About(){
           <div>Yo I&apos;m Nick and this is: </div>
           <div>Stuff I like to do</div>
         </div>
-        <div>{aboutMe("marathon", "/_art/marathon.jpg", "I ran a marathon in 4 hours and 35 minutes in my sophomore year of college in the winter. I was inspired by David Goggins to start this. This run was the day before all of my final exams which was super unlucky timing.")}</div>
+        <div>{aboutMe("marathon", "/_art/marathon.jpg", "I ran a marathon that raised money to eradicate child slavery in Ghana. I ran this in 4 hours and 35 minutes in my sophomore year of college in the winter. I was inspired by David Goggins to start this. This run was the day before all of my final exams which was super unlucky timing.")}</div>
         <div>{aboutMeReverse("volleyball", "/_art/vb.JPEG", "I play lots of volleyball at Texas A&M University and this is my intramural team. I got invested into volleyball because of a volleyball anime called Haikyuu!")}</div>
         <div>{aboutMe("basketball", "/_art/basketball.jpeg", "This is my intramural basketball team. I have played basketball since 7th grade and my favorite NBA player is Stephen Curry.")}</div>
         <div>{aboutMeReverse("piano", "/_art/piano.JPG", "I started learning how to play piano over quarantine and I currently know how to play 7 pieces.")}</div>
